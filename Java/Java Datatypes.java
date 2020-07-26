d1f23bfaf4e347ba2d1e2421c34f2c21336623d1
Saurabh Chandra <49842985+saurabhc24@users.noreply.github.com>
2020-07-26 23:47:16 +0530

    Create Java Datatypes.java

---
+++ Java/Java Datatypes.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t=sc.nextInt();
+    for(int i=0;i<t;i++)
+    {
+        try
+        {
+            long x=sc.nextLong();
+            System.out.println(x+" can be fitted in:");
+            if(x == (byte)x)System.out.println("* byte");                     /*Using type casting for conversions*/
+            if(x == (short)x)System.out.println("* short");                   
+            if(x == (int)x)System.out.println("* int");
+            if(x == (long)x)System.out.println("* long");
+        }
+        catch(Exception e)
+        {
+            System.out.println(sc.next()+" can't be fitted anywhere.");
+        }
+
+    }
+}
+}
